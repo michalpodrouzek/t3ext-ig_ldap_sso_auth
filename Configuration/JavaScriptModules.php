48906c24967ad97ab166c1e01b639671dc83230c
Michal Podrouzek <michal.podrouzek@devsk.io>
2024-09-06 15:00:58 +0200

    [FIX] TYPO3 12 support

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -6,6 +6,6 @@ return [
         'core',
     ],
     'imports' => [
-        '@causal/ig_ldap_sso_auth/' => 'EXT:ig_ldap_sso_auth/Resources/Public/JavaScript/',
+        '@michalpodrouzek/ig_ldap_sso_auth/' => 'EXT:ig_ldap_sso_auth/Resources/Public/JavaScript/',
     ],
 ];
